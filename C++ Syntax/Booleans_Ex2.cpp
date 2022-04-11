f1f0c7dc44b5d0de870d25da07556f2aec67bb66
laseronline <razeronline@hotmail.com>
2022-04-12 04:30:29 +0700

    Update Syntax

---
+++ C++ Syntax/Booleans_Ex2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    cout << (x == 10) << "\n";
+    cout << (10 > 9) << "\n"; // returns 1 (true), because 10 is higher than 9
+    cout << (10 == 15);
+    return 0;
+}
